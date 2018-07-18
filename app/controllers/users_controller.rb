bb3d69b2d5029da504de300d30d703a1382ccf02
Farzam <farzam.azher@square63.com>
2018-07-18 20:11:04 +0500

    Attachment: User profile picture add & update functionality implemented.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
   def set_profile_picture
+    if current_user.photo.present?
+      if current_user.photo.update(photo_params)
+        redirect_to edit_user_registration_path, notice: 'Profile picture is successfully updated'
+      else
+        redirect_to edit_user_registration_path, alert: 'Some Problem occured while updating Profile Picture.'
+      end
+    else
+      if current_user.create_photo(photo_params)
+        redirect_to edit_user_registration_path, notice: 'Profile picture is successfully updated'
+      else
+        redirect_to edit_user_registration_path, alert: 'Some Problem occured while updating Profile Picture.'
+      end
+    end
     
   end
+  
+  private
+    def photo_params
+      params.require(:user).require(:photo_attributes).permit(:data)
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def new_or_existing_photo(user)
+    if user.photo.nil?
+      user.build_photo
+    end
+  end
 end
