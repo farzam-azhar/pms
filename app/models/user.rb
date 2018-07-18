e195fa2697054f98731178ddf9de93ac72e10441
Farzam <farzam.azher@square63.com>
2018-07-18 13:42:03 +0500

    User: Added username, contact and gender fields to edit profile form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :username, length: { in: 6..15 }
-  validates :contact, format: { with: /((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/, multiline: true, message: 'g
-  iven format not supported.' }
+  validates :contact, format: { 
+  	with: /((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/, multiline: true, 
+		message: 'given format not supported.' 
+	}
   validates :gender, presence: true, inclusion: { in: %w(M F), message: 'You must select Male or Female Only.' }
 end
