9094e4b069e2148fbd15ac5c3206783cedecca40
Farzam <farzam.azher@square63.com>
2018-07-19 12:17:16 +0500

    Admin: Added admin attribute in users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180718115940) do
+ActiveRecord::Schema.define(version: 20180719070721) do
 
   create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "data_file_name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180718115940) do
     t.string "username"
     t.string "contact"
     t.string "gender", limit: 1
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
