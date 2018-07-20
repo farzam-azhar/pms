96c67bba03450365b4e661247d3e29b7046b53e1
Farzam <farzam.azher@square63.com>
2018-07-20 14:57:05 +0500

    Client: Implemented Crud functionality for clients.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,56 @@
 class ClientsController < ApplicationController
+  before_action :set_client, only: [:destroy, :edit, :update]
+  respond_to :html, :js
+  
   def index
+    @clients = Client.all
   end
+  
+  def new
+    @client = Client.new
+    @msg = "Create"
+    respond_with @client
+  end
+  
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      @check = true
+    else
+      @check = false
+    end
+    respond_with @client
+  end
+  
+  def edit
+    @msg = "Edit"
+    respond_with @client
+  end
+  
+  def update
+    if @client.update_attributes(client_params)
+      @check = true
+    else
+      @check = false
+    end
+    respond_with @client
+  end
+  
+  def destroy
+    if @client.destroy
+      respond_with @client
+    else
+      redirect_to clients_path, alert: "Something went Wrong while Deleting Client"
+    end
+  end
+  
+  private
+
+    def client_params
+      params.require(:client).permit(:name, :email, :country_code)
+    end
+    
+    def set_client
+      @client = Client.find params[:id]
+    end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,8 @@ class Client < ApplicationRecord
   validates :name, presence: true, length: { in: 5..15 }
   validates :email, presence: true, uniqueness: true, format: { with: Devise::email_regexp }
   validates :country_code, presence: true
+  
+  def country_name
+    country = ISO3166::Country[country_code]
+  end
 end
