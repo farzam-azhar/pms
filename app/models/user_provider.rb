fd263e894120188511d0aa3b938c45d5c15143cb
Farzam <farzam.azher@square63.com>
2018-08-07 10:07:50 +0500

    Refactoring: Refactored UserProvider model functions and removed extra gems of social icons.

--- app/models/user_provider.rb
+++ app/models/user_provider.rb
@@ -1,7 +1,7 @@
 class UserProvider < ApplicationRecord
   belongs_to :user
 
-  def self.find_for_facebook_oauth(auth)
+  def self.find_for_auth(auth)
     user = UserProvider.where(provider: auth.provider, uid: auth.uid).first
     unless user.nil?
       user.user
@@ -35,37 +35,11 @@ class UserProvider < ApplicationRecord
     end
   end
 
-    def self.find_for_google_oauth(auth)
-      user = UserProvider.where(provider: auth.provider, uid: auth.uid).first
-      unless user.nil?
-        user.user
-      else
-        registered_user = User.where(email: auth.info.email).first || User.where(username: auth.info.name).first
-        unless registered_user.nil?
-          UserProvider.create!(
-            provider: auth.provider,
-            uid: auth.uid,
-            user_id: registered_user.id
-          )
-          registered_user
-        else
-          gender ||= auth.extra.raw_info.gender
-          user = User.new(
-            username: auth.info.name,
-            email: auth.info.email,
-            gender: gender,
-            password: Devise.friendly_token[0,20],
-          )
-          user.provider = user.uid = true
-          user.save
+  def self.find_for_facebook_oauth(auth)
+    self.find_for_auth(auth)
+  end
 
-          user_provider = UserProvider.create!(
-            provider:auth.provider,
-            uid:auth.uid,
-            user: user
-          )
-          user
-        end
-      end
-    end
+  def self.find_for_google_oauth(auth)
+    self.find_for_auth(auth)
+  end
 end
