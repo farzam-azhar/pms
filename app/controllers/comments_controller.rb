c5c1f81ee2f7ee9cd37d1a80c0bac41d240d9986
Farzam <farzam.azher@square63.com>
2018-08-09 12:36:09 +0500

    Comment: Now Comments can be made on project index page as well.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_project, only: [:new, :create]
   before_action :find_comment, only: [:edit, :update, :destroy]
   before_action :validate_user, only: [:edit, :update, :destroy]
   respond_to :html, :js
 
+  def new
+    @comment = Comment.new
+    respond_to do |format|
+      format.js { render 'shared/new', locals: { object: @comment, modal: 'comment-modal-on-project-index', project: @project } }
+    end
+  end
+
   def create
-    @project = Project.find params[:project_id]
+    @check = params[:name] == 'latest_comment' ? true : false
     @comment = @project.comments.create(comment_params)
     respond_with @comment
   end
@@ -57,5 +65,9 @@ class CommentsController < ApplicationController
 
     def validate_user
       redirect_to project_path(@comment.project), alert: 'You are not authorized to perform this action.' if !@comment.valid_user?(current_user)
+    end
+
+    def find_project
+      @project = Project.find params[:project_id]
     end
 end
